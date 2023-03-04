12fdadfaffb65f2ad9686c192a3745e8bdac2386
saghargh <43346080+saghargh@users.noreply.github.com>
2023-03-04 12:32:06 +0330

    added icon

--- src/app.js
+++ src/app.js
@@ -23,19 +23,24 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(apiUrl);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "eo6cc869dfe854a6fa3b7d7tdda4b04f";
