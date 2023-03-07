ce9c618ee5701678a75346da5f6b71b737f551c4
saghargh <43346080+saghargh@users.noreply.github.com>
2023-03-07 18:46:36 +0330

    hours

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function changeBackground(hours) {
       .querySelector("body")
       .setAttribute("style", `background-image: url("./images/evening.jpg")`);
   }
-  if (hours > 20 && 0 < hours < 4) {
+  if (hours > 20 && hours < 4) {
     document
       .querySelector("body")
       .setAttribute("style", `background-image: url("./images/night.jpg")`);
